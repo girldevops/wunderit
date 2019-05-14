ce0dbdb57d03be8b21cdafdd8bfca50467fd59e0
Hajjar <devops.as.girl@gmail.com>
2019-05-14 11:16:47 +0200

    create first junit test

---
+++ src/test/java/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests {
+    
+    @Test
+    void testPassed() {
+        assertEquals(2, 2);
+    }
+    @Test
+    void testFail() {
+        assertEquals(2, 2);
+    }
+
+}
